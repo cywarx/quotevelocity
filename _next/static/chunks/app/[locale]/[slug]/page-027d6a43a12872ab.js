32da9f91446b08764cc39c50197a57e9d43899aa
Ankit-Ojha16 <Ankit.ojha.1432@gmail.com>
2024-04-24 12:33:12 +0530

    First Hit

---
+++ _next/static/chunks/app/[locale]/[slug]/page-027d6a43a12872ab.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[572],{},function(n){n.O(0,[756,414,910,968,959,688,39,167,881,768,744],function(){return n(n.s=8167)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/[locale]/legal/[slug]/page-4e6ec4497f10cc13.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[989],{8903:function(e,n,s){Promise.resolve().then(s.t.bind(s,568,23)),Promise.resolve().then(s.bind(s,688)),Promise.resolve().then(s.bind(s,3016)),Promise.resolve().then(s.bind(s,9262)),Promise.resolve().then(s.bind(s,932))}},function(e){e.O(0,[414,910,959,688,39,881,768,744],function(){return e(e.s=8903)}),_N_E=e.O()}]);
